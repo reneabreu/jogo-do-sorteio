214f607a0f4167d61490a0f74a7c9f298f8b418b
René Abreu <rene.abreu@outlook.com>
2018-08-25 15:58:30 -0300

    Get Button btn_Play

--- app/src/main/java/br/com/reneabreu/jogodosorteio/JogoDoSorteio.java
+++ app/src/main/java/br/com/reneabreu/jogodosorteio/JogoDoSorteio.java
@@ -2,6 +2,8 @@ package br.com.reneabreu.jogodosorteio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
 import java.util.Random;
 
 public class JogoDoSorteio extends AppCompatActivity {
@@ -10,6 +12,10 @@ public class JogoDoSorteio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jogo_do_sorteio);
+
+        // Get Button
+        Button btn_Play = findViewById(R.id.btn_Play);
+
     }
 
     private int GetRandomNumber() {
@@ -17,4 +23,5 @@ public class JogoDoSorteio extends AppCompatActivity {
 
         return randomNumber.nextInt(101);
     }
+
 }
