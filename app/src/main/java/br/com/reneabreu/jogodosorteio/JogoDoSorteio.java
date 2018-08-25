6bc2e849f5a9efc0045f2c0f0410dd949f8955b4
René Abreu <rene.abreu@outlook.com>
2018-08-25 16:10:48 -0300

    Button onClick event print result as a log

--- app/src/main/java/br/com/reneabreu/jogodosorteio/JogoDoSorteio.java
+++ app/src/main/java/br/com/reneabreu/jogodosorteio/JogoDoSorteio.java
@@ -2,6 +2,8 @@ package br.com.reneabreu.jogodosorteio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 import java.util.Random;
@@ -16,6 +18,13 @@ public class JogoDoSorteio extends AppCompatActivity {
         // Get Button
         Button btn_Play = findViewById(R.id.btn_Play);
 
+        // Set Button Behaviour
+        btn_Play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("Click Behaviour", "Clicked and got number:" + GetRandomNumber());
+            }
+        });
     }
 
     private int GetRandomNumber() {
